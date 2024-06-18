40af428341732485dd346dba5d18da1109dac1c7
Sani <alfey2001sani@gmail.com>
2024-06-18 19:49:04 +0600

    Initial Commit

---
+++ src/test/java/dev/sani/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sani.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
